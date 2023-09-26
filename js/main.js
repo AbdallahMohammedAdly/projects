be3076fb898518b5044e7e2e1e3b6c75ab16100c
AbdallahMohammedAdly <abdallahedly@gmail.com>
2023-09-26 19:44:47 +0300

    first html , css and js wlecome site

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// just take name from user then display massage "welcome  + name "
+let elm = document.getElementById('demo');
+
+
+let name = prompt('Enter your name :');
+
+elm.innerText = 'Welcome ' + name ;
